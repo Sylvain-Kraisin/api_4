ffd430f9edc648200bcc1923671cebfd6557d769
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-03-22 21:49:49 +0100

    files ok 'Hello World'

--- api_4.rb
+++ api_4.rb
@@ -1,1 +1,16 @@
-run Proc.new { |env| ['200', {'Content-Type' => 'text/html'}, ['get rack\'d']] }
+require 'rack'
+require 'rack/server'
+
+class HelloWorld
+  def response
+    [200, {}, 'Hello World']
+  end
+end
+
+class HelloWorldApp
+  def self.call(env)
+    HelloWorld.new.response
+  end
+end
+
+Rack::Server.start :app => HelloWorldApp
